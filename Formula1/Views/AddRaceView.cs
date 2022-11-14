515c8b8dec2ad5ecedb55ecd680d93aa8f05a6c3
Ivo Dimitrov <ivaylo.dimitrov93@gmail.com>
2022-11-15 00:36:33 +0100

    AddRaceView added

---
+++ Formula1/Views/AddRaceView.cs
@@ -0,0 +1,25 @@
+﻿using Formula1.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Formula1.Views
+{
+    public class AddRaceView
+    {
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(15)]
+        public string Country { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        public int RaceTypeId { get; set; }
+
+        public IEnumerable<RaceType> RaceTypes { get; set; } = new List<RaceType>();
+
+    }
+}
