41e5187593d5ff79ae23a858477299b562803e2a
Ivo Dimitrov <ivaylo.dimitrov93@gmail.com>
2022-11-14 15:21:23 +0100

    ErrorView added

---
+++ Formula1/Views/ErrorView.cs
@@ -0,0 +1,9 @@
+﻿namespace Formula1.Views
+{
+    public class ErrorView
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
