70d53f7936f6a5350ea62fa369d440204a313d4d
Ivo Dimitrov <ivaylo.dimitrov93@gmail.com>
2022-11-14 15:15:32 +0100

    RegisterView added

---
+++ Formula1/Views/RegisterView.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formula1.Views
+{
+    public class RegisterView
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(60, MinimumLength = 10)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
