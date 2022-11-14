cde50bce60170aaf89e313619153e73867e99fc1
Ivo Dimitrov <ivaylo.dimitrov93@gmail.com>
2022-11-14 22:04:16 +0100

    AddConstructorView added

---
+++ Formula1/Views/AddConstructorView.cs
@@ -0,0 +1,21 @@
+﻿using Formula1.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Formula1.Views
+{
+    public class AddConstructorView
+    {        
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(20)]
+        public string EngineSupplier { get; set; } = null!;
+
+        public int CountryId { get; set; }
+
+        public List<Driver> Drivers { get; set; } = new List<Driver>();
+    }
+}
